453a9fb17bf2deb034818d436f1817650b9033f2
Brandon Fiebiger <brandonfiebiger@gmail.com>
2019-01-12 12:41:50 -0700

    Add window.onload function, Break out logic into addToDom function

--- vanilla-js-practice/ideabox/script.js
+++ vanilla-js-practice/ideabox/script.js
@@ -4,12 +4,21 @@ const contentInput = document.querySelector('.content-input');
 const submitButton = document.querySelector('.submit-button');
 const ideaList = document.querySelector('.idea-list');
 
+window.onload = () => {
+  const ideasArray = JSON.parse(localStorage.getItem('ideas'));
+  ideasArray.forEach(idea => addToDom(idea));
+};
+
 ideaForm.addEventListener('submit', (e) => {
   e.preventDefault();
   const idea = new Idea(titleInput.value, contentInput.value);
+  addToDom(idea)
+  Idea.addIdea(idea);
+  ideaForm.reset();
+});
+
+const addToDom = (idea) => {
   const ideaElement = document.createElement('li');
   ideaElement.innerHTML = idea.markup;
   ideaList.prepend(ideaElement);
-  Idea.addIdea(idea);
-  ideaForm.reset();
-})
+}
