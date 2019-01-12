90832c29aaed9180e97b38957c4be8deb739f334
Brandon Fiebiger <brandonfiebiger@gmail.com>
2019-01-12 12:15:41 -0700

    Add markup as a property on the Idea class, add Ideas to localStorage

--- vanilla-js-practice/ideabox/idea.js
+++ vanilla-js-practice/ideabox/idea.js
@@ -4,10 +4,18 @@ class Idea {
     this.content = content;
     this.id = Date.now();
     this.quality = 'swill';
+    this.markup = `
+      <h2>${this.title}</h2>
+      <p>${this.content}</p>
+      <p>${this.quality}</p>
+      <button data-id=${this.id}>X</button>
+  `;
   }
 
 
   static addIdea(idea) {
-    console.log(idea)
+    let ideasArray = JSON.parse(localStorage.getItem('ideas')) || [];
+    ideasArray = [idea, ...ideasArray];
+    localStorage.setItem('ideas', JSON.stringify(ideasArray));
   }
 }
