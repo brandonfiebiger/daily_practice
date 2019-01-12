652c6c4b71994ea424cf02aa3276caebe42e3218
Brandon Fiebiger <brandonfiebiger@gmail.com>
2019-01-12 13:47:56 -0700

    Add markup back to addToDom function and listen for event on upvote and downvote buttons

--- vanilla-js-practice/ideabox/script.js
+++ vanilla-js-practice/ideabox/script.js
@@ -21,11 +21,26 @@ ideaList.addEventListener('click', (e) => {
   if (e.target.className === 'delete-button') {
     e.target.parentElement.remove();
     Idea.deleteIdea(e.target.dataset.id);
+  } else if (e.target.className === 'upvote' || e.target.className === 'downvote') {
+    const newQuality = Idea.updateQuality(e);
+    console.log(newQuality);
+    e.target.parentElement.previousSibling.previousSibling.innerText = newQuality;
   }
 })
 
 const addToDom = (idea) => {
   const ideaElement = document.createElement('li');
-  ideaElement.innerHTML = idea.markup;
+
+  ideaElement.innerHTML = `
+    <h2 class="idea-title">${idea.title}</h2>
+    <p class="idea-content">${idea.content}</p>
+    <p class="idea-quality">${idea.quality}</p>
+    <div class="vote-buttons" data-id=${idea.id}>
+      <button class="upvote">&uarr;</button>
+      <button class="downvote">&darr;</button>
+    </div>
+    <button class="delete-button" data-id=${idea.id}>X</button>
+`;
+
   ideaList.prepend(ideaElement);
 }
