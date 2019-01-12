3d5b2032852b5e24689c11fd69ea73692b071322
Brandon Fiebiger <brandonfiebiger@gmail.com>
2019-01-12 13:47:08 -0700

    Delete markup as property on idea class, add updateQuality function

--- vanilla-js-practice/ideabox/idea.js
+++ vanilla-js-practice/ideabox/idea.js
@@ -1,15 +1,9 @@
 class Idea {
-  constructor(title, content) {
+  constructor(title, content, quality) {
     this.title = title;
     this.content = content;
     this.id = Date.now();
-    this.quality = 'swill';
-    this.markup = `
-      <h2 class="idea-title">${this.title}</h2>
-      <p class="idea-content">${this.content}</p>
-      <p class="idea quality">${this.quality}</p>
-      <button class="delete-button" data-id=${this.id}>X</button>
-  `;
+    this.quality = quality || 'swill';
   }
 
 
@@ -23,5 +17,39 @@ class Idea {
     let ideasArray = JSON.parse(localStorage.getItem('ideas'));
     ideasArray = ideasArray.filter(idea => idea.id != id);
     localStorage.setItem('ideas', JSON.stringify(ideasArray));
+  }
+
+  static updateQuality(e) {
+    const upArrow = {
+      'swill': 'great',
+      'great': 'extraordinary',
+      'extraordinary': 'extraordinary'
+    };
+    const downArrow = {
+      'extraordinary': 'great',
+      'great': 'swill',
+      'swill': 'swill'
+    };
+
+    let ideasArray = JSON.parse(localStorage.getItem('ideas'));
+    let newQuality;
+    ideasArray.forEach(idea => {
+      if (idea.id == e.target.parentElement.dataset.id) {
+        switch(e.target.className) {
+          case 'upvote':
+            newQuality = upArrow[idea.quality];
+            idea.quality = upArrow[idea.quality];
+            break;
+          case 'downvote':
+            newQuality = downArrow[idea.quality];
+            idea.quality = downArrow[idea.quality];
+            break;
+          default:
+            return;
+        }
+      }
+    })
+    localStorage.setItem('ideas', JSON.stringify(ideasArray));
+    return newQuality;
   }
 }
