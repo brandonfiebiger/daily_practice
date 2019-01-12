9f41207c44400084377ac2ab3b88769431be3387
Brandon Fiebiger <brandonfiebiger@gmail.com>
2019-01-12 11:55:16 -0700

    Declare global variables and addEventListener to ideaForm

---
+++ vanilla-js-practice/ideabox/script.js
@@ -0,0 +1,13 @@
+const ideaForm = document.querySelector('.idea-form');
+const titleInput = document.querySelector('.title-input');
+const contentInput = document.querySelector('.content-input');
+const submitButton = document.querySelector('.submit-button');
+const ideaList = document.querySelector('.idea-list');
+
+ideaForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const idea = new Idea(titleInput.value, contentInput.value);
+  const ideaElement = document.createElement('li');
+  Idea.addIdea(idea);
+  ideaForm.reset();
+})
