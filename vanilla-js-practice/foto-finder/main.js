27c5f20f769a32194d2c6888c40aa387aa6eb4d4
Brandon Fiebiger <brandonfiebiger@gmail.com>
2019-01-13 16:20:02 -0700

    Refactor event listener for photo selector

--- vanilla-js-practice/foto-finder/main.js
+++ vanilla-js-practice/foto-finder/main.js
@@ -2,7 +2,9 @@ const selectFile = document.querySelector('.select-file');
 const preview = document.querySelector('.preview');
 
 
-selectFile.addEventListener('change', () => {
+selectFile.addEventListener('change', () => selectAndPreviewFile())
+
+const selectAndPreviewFile = () => {
   const reader = new FileReader();
   const file = selectFile.files[0];
   reader.addEventListener('load', () => {
@@ -12,4 +14,6 @@ selectFile.addEventListener('change', () => {
   if(file) {
     reader.readAsDataURL(file);
   }
-})
+}
+
+
