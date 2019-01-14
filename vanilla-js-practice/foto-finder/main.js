ad017b225353d2ba5273349956c301e531c616dd
Brandon Fiebiger <brandonfiebiger@gmail.com>
2019-01-13 17:26:07 -0700

    Successfully create instances of photo from dom

--- vanilla-js-practice/foto-finder/main.js
+++ vanilla-js-practice/foto-finder/main.js
@@ -1,14 +1,15 @@
 const selectFile = document.querySelector('.select-file');
 const preview = document.querySelector('.preview');
 const addPhotoForm = document.querySelector('.add-photo-form');
+const photoList = document.querySelector('.photo-list');
 const titleInput = document.querySelector('.title-input');
 const captionInput = document.querySelector('.caption-input');
+const reader = new FileReader();
 
 selectFile.addEventListener('change', () => selectAndPreviewFile());
 addPhotoForm.addEventListener('submit', (e) => handleAddPhoto(e));
 
 const selectAndPreviewFile = () => {
-  const reader = new FileReader();
   const file = selectFile.files[0];
   reader.addEventListener('load', () => {
     preview.src = reader.result;
@@ -21,17 +22,20 @@ const selectAndPreviewFile = () => {
 
 const handleAddPhoto = (e) => {
   e.preventDefault();
-  const reader = new FileReader();
-  console.log(reader.result);
   const title = titleInput.value;
   const caption = captionInput.value;
   const file = selectFile.files[0];
-  if (!title || !caption || !file) {
+  if (!title | !caption || !file) {
     return;
   } else {
     const photo = new Photo(title, caption, file);
     Photo.saveToStorage(photo);
   }
 }
+
+
+
+
+
 
 
