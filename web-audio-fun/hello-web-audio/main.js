590e3a330883aaeb3c7550bec4f65f6a0e0b8896
Brandon Fiebiger <brandonfiebiger@gmail.com>
2019-01-18 14:34:41 -0700

    Create delay function, add some setTimeOuts for a lil sequence.

--- web-audio-fun/hello-web-audio/main.js
+++ web-audio-fun/hello-web-audio/main.js
@@ -1,28 +1,43 @@
 const playButton = document.querySelector('.play-sounds');
 const stopButton = document.querySelector('.stop-sounds');
+const delayButton = document.querySelector('.add-delay');
 
 const audioCtx = new (window.AudioContext || window.webkitAudioContext);
+const synthDelay = audioCtx.createDelay(10.0);
 
 const sineA = audioCtx.createOscillator();
 sineA.frequency.value = 240;
 sineA.type = "sine";
 
 const sineB = audioCtx.createOscillator();
-sineB.frequency.value = 723.25;
+sineB.frequency.value = 333.25;
 sineB.type = "sine";
 
 const sineC = audioCtx.createOscillator();
-sineC.frequency.value = 689.46;
+sineC.frequency.value = 289.46;
 sineC.type = "sine";
 
 
 const playSounds = () => {
   sineA.start()
   sineA.connect(audioCtx.destination);
-  sineB.start()
-  sineB.connect(audioCtx.destination);
-  sineC.start()
-  sineC.connect(audioCtx.destination);
+  setTimeout(() => {
+    sineA.stop();
+    sineB.start()
+    sineB.connect(audioCtx.destination);
+  }, 5000);
+
+  setTimeout(() => {
+    sineB.stop();
+    sineC.start()
+    sineC.connect(audioCtx.destination);
+  }, 10000);
+}
+
+const startDelay = () => {
+  sineA.connect(synthDelay);
+  sineB.connect(synthDelay);
+  synthDelay.connect(audioCtx.destination);
 }
 
 const stopSounds = () => {
@@ -33,3 +48,4 @@ const stopSounds = () => {
 
 playButton.addEventListener('click', playSounds);
 stopButton.addEventListener('click', stopSounds);
+delayButton.addEventListener('click', startDelay);
