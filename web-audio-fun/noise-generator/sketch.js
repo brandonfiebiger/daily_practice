c70e6be90b8f0e2373b9b18ad4ec992db8c16b19
Brandon Fiebiger <brandonfiebiger@gmail.com>
2019-01-19 16:12:41 -0700

    Map the frequencies and scale the spectrum to the canvas

--- web-audio-fun/noise-generator/sketch.js
+++ web-audio-fun/noise-generator/sketch.js
@@ -43,9 +43,8 @@ function setup() {
 function draw() {
 	background('black');
 	let spectrum = fft.analyze();
-	let counter = 0;
-	
+
 	for (let i = 0; i < spectrum.length; i++) {
-		point(i, spectrum[i]);
+		point(map(i, 0, spectrum.length, 0, width), map(spectrum[i], 0, 255, height, 0));
 	}
 }
