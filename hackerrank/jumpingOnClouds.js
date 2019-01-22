cc5b3d2e018774724b97b71f3af4622d5bc34a6a
Brandon Fiebiger <brandonfiebiger@gmail.com>
2019-01-22 14:38:44 -0700

    Fix issue with jumpingOnClouds function.

--- hackerrank/jumpingOnClouds.js
+++ hackerrank/jumpingOnClouds.js
@@ -1,15 +1,14 @@
 const jumpingOnClouds = clouds => {
   let counter = 0;
 
-  for (let i = 0; i <= clouds.length; i++) {
-      if (clouds[i + 2] === 0) {
-          i = i + 2;
+  for (let i = 0; i < clouds.length - 1; i++) {
+      if (clouds[i + 2] < 1) {
+          i++;
           counter++
       } else {
           counter++;
       }
   }
-
   return counter
 
 }
