d2bd455fd578f4c47ba22bc7a9da1c8596eb8955
Brandon Fiebiger <brandonfiebiger@gmail.com>
2019-01-26 14:41:20 -0700

    Add eventListener for  on load to prepend stored ideas to the DOM and break up addIdea function into two functions

--- vanilla-js-practice/extra-ideabox/main.js
+++ vanilla-js-practice/extra-ideabox/main.js
@@ -3,23 +3,32 @@ const bodyInput = document.querySelector('.body-input');
 const ideaForm = document.querySelector('.idea-form');
 const ideaList = document.querySelector('.idea-list');
 
-const addIdea = (e) => {
+window.addEventListener('load', () => {
+  const ideasArray = JSON.parse(localStorage.getItem('ideas'));
+
+  ideasArray.forEach(idea => {
+    addIdeaToDom(idea);
+  })
+})
+
+const handleAddIdea = (e) => {
   e.preventDefault();
   const title = titleInput.value;
   const body = bodyInput.value;
-  
   const idea = new Idea(title, body);
-  
-  const ideaCard = document.createElement('li');
-  ideaCard.className = 'idea-card';
-  ideaCard.innerHTML = `
-    <h2>${title}</h2>
-    <p>${body}</p>
-  `;
-
-  ideaList.prepend(ideaCard);
-  ideaForm.reset();
+  addIdeaToDom(idea)
   Idea.addIdea(idea);
 }
 
-ideaForm.addEventListener('submit', addIdea);
+const addIdeaToDom = (idea) => {
+  const ideaCard = document.createElement('li');
+  ideaCard.className = 'idea-card';
+  ideaCard.innerHTML = `
+    <h2>${idea.title}</h2>
+    <p>${idea.body}</p>
+  `;
+
+  ideaList.prepend(ideaCard);
+}
+
+ideaForm.addEventListener('submit', handleAddIdea);
