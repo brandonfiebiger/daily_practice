3205403a53fc0afff58d0aecbc8b3599aed2bd29
Brandon Fiebiger <brandonfiebiger@gmail.com>
2019-01-27 15:00:20 -0700

    Add deleteIdea function on Idea class

--- vanilla-js-practice/extra-ideabox/idea.js
+++ vanilla-js-practice/extra-ideabox/idea.js
@@ -11,4 +11,10 @@ class Idea {
     ideasArray = [...ideasArray, idea];
     localStorage.setItem('ideas', JSON.stringify(ideasArray));
   }
+
+  static deleteIdea(id) {
+    let ideasArray = JSON.parse(localStorage.getItem('ideas'));
+    ideasArray = ideasArray.filter(idea => idea.id != id);
+    localStorage.setItem('ideas', JSON.stringify(ideasArray));
+  }
 }
