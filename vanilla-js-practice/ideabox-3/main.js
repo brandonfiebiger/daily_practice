abef0fd5271b202740cb6032324733c4e4fe2ca1
Brandon Fiebiger <brandonfiebiger@gmail.com>
2019-02-01 14:35:53 -0700

    Select html elements and store in variables. Add handleAddIdea and prependIdea functions

---
+++ vanilla-js-practice/ideabox-3/main.js
@@ -0,0 +1,29 @@
+const titleInput = document.querySelector('.title-input');
+const bodyInput = document.querySelector('.body-input');
+const ideaForm = document.querySelector('.idea-form');
+const ideaList = document.querySelector('.idea-list');
+
+const handleAddIdea = (e) => {
+  e.preventDefault();
+  const title = titleInput.value;
+  const body = bodyInput.value;
+  const idea = new Idea(title, body);
+  prependIdea(idea);
+  // Idea.addIdea(idea);
+}
+
+const prependIdea = (idea) => {
+  const li = document.createElement('li');
+  li.innerHTML = `
+    <h2>${idea.title}</h2>
+    <p>${idea.body}</p>
+    <button class="up-arrow">&uarr;</button>
+    <button class="down-arrow">&darr;</button>
+    <p>${idea.quality}</p>
+    <button class="delete-button">x</button>
+`
+  ideaList.prepend(li);
+}
+
+ideaForm.addEventListener('submit', handleAddIdea);
+
