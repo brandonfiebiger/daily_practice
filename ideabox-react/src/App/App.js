11ed45428961002278a1d98114106a8ac3698aac
Brandon Fiebiger <brandonfiebiger@gmail.com>
2019-02-12 12:53:01 -0700

    Import IdeaForm into App and add to markup.

--- ideabox-react/src/App/App.js
+++ ideabox-react/src/App/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import { IdeaForm } from '../IdeaForm/IdeaForm';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
      <div>
-       
+       <IdeaForm />
      </div>
     );
   }
