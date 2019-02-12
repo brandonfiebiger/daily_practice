e659306df2cee7e8be9aab68cfae96a48d0b2233
Brandon Fiebiger <brandonfiebiger@gmail.com>
2019-02-12 13:29:51 -0700

    Import IdeaCard into IdeaList. Map over ideas and display them in the list.

--- ideabox-react/src/IdeaList/IdeaList.js
+++ ideabox-react/src/IdeaList/IdeaList.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { IdeaCard } from '../IdeaCard/IdeaCard';
 
-export const IdeaList = () => {
+export const IdeaList = (props) => {
+  
+  const displayIdeas = () => {
+    return props.ideas.map(idea => <IdeaCard title={idea.title} body={idea.body} id={Date.now} />)
+  }
 
+  
   return (
     <ul>
-
+      { displayIdeas() }
     </ul>
   )
 }
