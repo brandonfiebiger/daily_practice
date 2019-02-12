a33b26f02e9b7200d869a494bc4b31e0204c32f8
Brandon Fiebiger <brandonfiebiger@gmail.com>
2019-02-12 13:15:42 -0700

    Add state to IdeaForm with title and body and add handleAddIdea function.

--- ideabox-react/src/IdeaForm/IdeaForm.js
+++ ideabox-react/src/IdeaForm/IdeaForm.js
@@ -2,15 +2,24 @@ import React, { Component } from 'react';
 
 
 export class IdeaForm extends Component {
-  constructor() {
+  constructor(props) {
     super();
 
+    this.state = {
+      title: "",
+      body: ""
+    }
+  }
+
+  handleAddIdea = (e) => {
+    e.preventDefault();
+    this.props.addIdea(this.state);
   }
 
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleAddIdea} >
         <input type="text" placeholder="title"/>
         <input type="text" placeholder="body"/>
         <button type="submit">Add Idea</button>
