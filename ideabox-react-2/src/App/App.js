93bf5dcfddb5dc3ae3bf1e73904f1d3a73a5a2ad
Brandon Fiebiger <brandonfiebiger@gmail.com>
2019-02-15 18:21:12 -0700

    Import IdeaForm and add to markup.

--- ideabox-react-2/src/App/App.js
+++ ideabox-react-2/src/App/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import { IdeaForm } from '../IdeaForm/IdeaForm';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
-        
+        <IdeaForm />
       </React.Fragment>
     );
   }
